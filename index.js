cf2473345a643d6223f03f3fd6dc65500c40eb82
sasha wangui <sasha.iluku@gmail.com>
2023-06-15 23:32:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,36 +3,38 @@
 const cats = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendCat(name) {
-    cats.push(name)
+    cats.push("Ralph")
 }
 
 function destructivelyPrependCat(name) {
-    cats.unshift(name)
+    cats.unshift("Bob")
 }
 
 function destructivelyRemoveLastCat() {
-    cats.length = 2
+    cats.pop(2)
 }
 
 function destructivelyRemoveFirstCat() {
-    cats.shift()
+    cats.shift(0)
 }
 
 function appendCat(name) {
     let newArray = [...cats, name]
     return newArray
-
 }
+console.log('Broom')
 
 function prependCat(name) {
     let newArray = [name, ...cats]
     return newArray;
 }
+console.log('Arnold')
 
 function removeLastCat() {
     let newArray = cats.slice(0, -1)
     return newArray;
 }
+
 
 function removeFirstCat() {
     let newArray = cats.slice(1)
